01ae5ee599a733fb3f65b7867976ce5d41144376
Cameron Larsen <camjlarsen@gmail.com>
2023-03-13 17:13:44 -0700

    feat: Add text objects and vim-surround like keybinds
    - Adds text objects for quotes and braces/brackets
    - Adds vim-surround like keybinds to change, delete, and add characters

--- vim.plugin.zsh
+++ vim.plugin.zsh
@@ -1,6 +1,6 @@
 #!/bin/sh
 bindkey -v
-export KEYTIMEOUT=1
+export KEYTIMEOUT=25
 
 if [[ -o menucomplete ]]; then 
   # Use vim keys in tab complete menu:
@@ -28,3 +28,26 @@ zle-line-init() {
 zle -N zle-line-init
 echo -ne '\e[5 q' # Use beam shape cursor on startup.
 preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+
+# Add text objects for quotes and brackets.
+autoload -Uz select-bracketed select-quoted
+zle -N select-quoted
+zle -N select-bracketed
+for m in viopp visual; do
+  for c in {a,i}{\',\",\`}; do
+      bindkey -M $m -- $c select-quoted
+  done
+  for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+      bindkey -M $m -- $c select-bracketed
+  done
+done
+
+# Add surround like commands.
+autoload -Uz surround
+zle -N delete-surround surround
+zle -N add-surround surround
+zle -N change-surround surround
+bindkey -M vicmd cs change-surround
+bindkey -M vicmd ds delete-surround
+bindkey -M vicmd ys add-surround
+bindkey -M visual S add-surround
