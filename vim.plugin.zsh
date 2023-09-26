46284178bcad362db40509e1db058fe78844d113
Cameron Larsen <44081018+cameronjlarsen@users.noreply.github.com>
2023-09-26 01:45:34 -0700

    feat: Add text objects and vim-surround like keybinds (#5)
    - Adds text objects for quotes and braces/brackets
    - Adds vim-surround like keybinds to change, delete, and add characters
    
    Co-authored-by: Marco <82162277+mamaraddio@users.noreply.github.com>

--- vim.plugin.zsh
+++ vim.plugin.zsh
@@ -33,6 +33,28 @@ preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
 bindkey -M viins '^A' beginning-of-line 
 bindkey -M viins '^E' end-of-line 
 
+# Add text objects for quotes and brackets.
+autoload -Uz select-bracketed select-quoted
+zle -N select-quoted
+zle -N select-bracketed
+for m in viopp visual; do
+  for c in {a,i}{\',\",\`}; do
+      bindkey -M $m -- $c select-quoted
+  done
+  for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+      bindkey -M $m -- $c select-bracketed
+  done
+done
+
+# Add surround like commands.
+autoload -Uz surround
+zle -N delete-surround surround
+zle -N add-surround surround
+zle -N change-surround surround
+bindkey -M vicmd cs change-surround
+bindkey -M vicmd ds delete-surround
+bindkey -M vicmd ys add-surround
+bindkey -M visual S add-surround
 # escape back into normal mode
 if [[ -n "${VI_MODE_ESC_INSERT}" ]] then
     bindkey -M viins "${VI_MODE_ESC_INSERT}" vi-cmd-mode
